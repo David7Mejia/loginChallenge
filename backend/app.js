fc587186c505897a4ef59e8968608ddd21bd0f51
David Mejia <david7mejia@gmail.com>
2022-03-13 18:59:36 -0500

    setup routes and middleware

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const csurf = require("csurf");
 const cookieParser = require("cookie-parser");
-
+const parse = require('csv-parser');
+const csrf = require("csurf");
+const fs = require('fs')
 //initiliaze express
 const app = express();
 
@@ -12,6 +13,32 @@ app.use(express.json()); //middleware for parsing json
 
 //initialize cors
 app.use(cors()); //middleware for enabling cors (Cross-Origin Resource Sharing)
+
+app.use(csrf({
+  cookie: true,
+}))
+
+let csv_data = {};
+
+fs.createReadStream("./logindata.csv")
+  .pipe(parse())
+  .on('data', (csvrow) => {
+    try {
+      csv_data[csvrow.Id] = csvrow;
+    }
+    catch (error) {
+      console.log(error)
+    }
+  })
+  .on('end', function () {
+    console.log('this is csv data', csv_data)
+    console.log('CSV file successfully processed');
+  });
+
+app.get('/', (req, res) => {
+    res.send(csv_data[1])
+});
+
 
 const port = 5000;
 
---
+++ backend/routes/utils.js
@@ -0,0 +1,9 @@
+const csrf = require("csurf");
+const asyncHandler = require("express-async-handler");
+
+const csrfProtection = csrf({cookie: true})
+
+module.exports = {
+    csrfProtection,
+    asyncHandler
+}
